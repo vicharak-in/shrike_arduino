81480046015c310a44a38d2b729ef559699a120e
dpks2003 <dshardan007@gmail.com>
2025-11-04 18:19:21 +0530

    add shrike-arduino library

---
+++ examples/shrike_flash/shrike_flash.ino
@@ -0,0 +1,17 @@
+#include "Shrike.h"
+
+ShrikeFlash shrike;
+
+void setup() {
+  Serial.begin(115200);
+  while (!Serial && millis() < 3000);
+
+  shrike.begin();
+  shrike.flash("/led_blink.bin"); // replace with your file name
+}
+
+void loop() {
+  // Empty loop - nothing to do
+}
+
+
